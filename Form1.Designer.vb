2bd715792b5d4a6e9813d2c626851e7d612815f2
FanyFadhillahR3 <103325576+FanyFadhillahR3@users.noreply.github.com>
2023-04-13 23:24:28 +0700

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.Label()
+        Me.txtMinuman = New System.Windows.Forms.TextBox()
+        Me.txtKeterangan = New System.Windows.Forms.TextBox()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(21, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(74, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Minuman"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.AutoSize = True
+        Me.TextBox2.Location = New System.Drawing.Point(21, 135)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(92, 20)
+        Me.TextBox2.TabIndex = 1
+        Me.TextBox2.Text = "Keterangan"
+        '
+        'txtMinuman
+        '
+        Me.txtMinuman.Location = New System.Drawing.Point(25, 47)
+        Me.txtMinuman.Name = "txtMinuman"
+        Me.txtMinuman.Size = New System.Drawing.Size(247, 26)
+        Me.txtMinuman.TabIndex = 2
+        '
+        'txtKeterangan
+        '
+        Me.txtKeterangan.Location = New System.Drawing.Point(25, 174)
+        Me.txtKeterangan.Name = "txtKeterangan"
+        Me.txtKeterangan.Size = New System.Drawing.Size(629, 26)
+        Me.txtKeterangan.TabIndex = 3
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(397, 47)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(96, 28)
+        Me.btnSubmit.TabIndex = 4
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(486, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(197, 20)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Fany Fadhillah 201511029"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(788, 244)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.txtKeterangan)
+        Me.Controls.Add(Me.txtMinuman)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox2 As Label
+    Friend WithEvents txtMinuman As TextBox
+    Friend WithEvents txtKeterangan As TextBox
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        Select Case txtMinuman.Text
+            Case "JP" : TextBox2.Text = "Jeruk Peras"
+
+            Case "MNL" : TextBox2.Text = "Mineral"
+
+            Case "JA" : TextBox2.Text = "Jus Apel"
+
+            Case Else : TextBox2.Text = "Tomat"
+        End Select
+    End Sub
+End Class
